116d17ecfedb278fc3924a5743698581a325ac8f
Nala Mahen <mail2mahen@yahoo.co.uk>
2018-06-20 16:29:13 +0100

    Add test for post /todos route.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -11,19 +10,19 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then(doc => {
-        res.send(doc);
-    }, e => {
-        res.status(400).send(e);
-    })
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Started on port 3000');
-})
+  console.log('Started on port 3000');
+});
 
-
+module.exports = {app};
