ee8501002ba60efd8e54dfdc91b1e6da4045c684
Nala Mahen <mail2mahen@yahoo.co.uk>
2018-06-21 16:56:59 +0100

    Change port from 3000 3080

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.eventNames.PORT || 3000;
+const port = process.eventNames.PORT || 3080;
 
 app.use(bodyParser.json());
 
